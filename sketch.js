fe1b2f127f6133450af7b271a61605a7ce7650d3
Madhavan <madhavanraja99@gmail.com>
2019-05-07 19:44:27 +0530

    Used global velocity

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
-var dinoGraphics;
+var dino;
 var cactus = [];
+var ground;
 
 let score = 0;
 let highScore = 0;
 
+let movementVelocity = -5;
+
 function setup()
 {
-	createCanvas(800, 300);
+	createCanvas(800, 200);
+	ground = new Ground();
 	dino = new Dino();
 
 	cactus.push(new Cactus());
@@ -14,38 +18,38 @@ function setup()
 
 function draw()
 {
-	background(51);
+	background(255);
+
+	ground.update();
+	ground.show();
 
 	dino.update();
-	rect(dino.x, dino.y - dino.height, dino.width, dino.height);
-
 	dino.show();
 
 	if (frameCount % 150 == 0)
 		cactus.push(new Cactus());
 
-		for (let i = cactus.length - 1; i >= 0; i--)
-		{
-			cactus[i].update();
-			cactus[i].show();
-	
-			if (cactus[i].offscreen())
-			{
-				cactus.splice(i, 1);
+	for (let i = cactus.length - 1; i >= 0; i--)
+	{
+		cactus[i].update();
+		cactus[i].show();
 
-				score += 1;
-				if (score > highScore)
-					highScore = score;
-			}
-	
-			if (cactus[i].hits(dino))
-			{
-				console.log("Hit");
-				dino.reset();
-				cactus = [];
-				score = 0;
-			}
+		if (cactus[i].offscreen())
+		{
+			cactus.splice(i, 1);
+
+			score += 1;
+			if (score > highScore)
+				highScore = score;
 		}
+
+		if (cactus[i].hits(dino))
+		{
+			dino.reset();
+			cactus = [];
+			score = 0;
+		}
+	}
 }
 
 function keyPressed()
